e0b0bad192c65e0b8a57f0ae55026e289997a312
JuliaDiTomas <111463451+JuliaDiTomas@users.noreply.github.com>
2024-10-31 15:46:37 -0600

    Update test_file_upload.ino

--- test_file_upload.ino
+++ test_file_upload.ino
@@ -1,5 +1,6 @@
 #include <SPIFFS.h>
 #include <WiFi.h>
+#include <StreamLib.h>
 
 // Replace with your network credentials
 const char* ssid = "ESP32-Access-Point";
@@ -49,31 +50,39 @@ void loop() {
             Serial.println("Received request");
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
             // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            // Send your "Hello World" HTML response
-            client.println("<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
-            client.println("<body><p><a href=\"/download\"><button class=\"button button2\">Download</button></a></p></body></html>");
-            //client.println("<body><p><a href=\"test.txt\">Download!</a></p></body></html>");
-
+           
             // The HTTP response ends with another blank line
             client.println();
             Serial.print(request);
             Serial.println("is the request");
             if (request.indexOf("GET /download") >= 0) {
               Serial.println("Download Requested");
-              File dataFile = SPIFFS.open("/test.txt", FILE_READ);
+              File dataFile = SPIFFS.open("/test.csv", FILE_READ);
               int filesize = dataFile.size();
-              Serial.println("Content-Length:"+String(filesize));
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-Type: text/plain");
-              client.println("Content-Length:"+String(filesize));
-              client.println("Connection: close");
-              client.println();
-              client.write(dataFile);
+              // Serial.println("Content-Length:"+String(filesize));
+              // client.println("HTTP/1.1 200 OK");
+              // client.println("Content-Type: text/plain");
+              // client.println("Content-Length:"+String(filesize));
+              // client.println("Connection: close");
+              // client.println();
+              // client.write(dataFile);
+
+              char buff[1024];
+
+              ChunkedPrint bp(client, buff, sizeof(buff));
+              bp.println(F("HTTP/1.1 200 OK"));
+              bp.println(F("Connection: close"));
+              bp.print(F("Content-Length: "));
+              bp.println(dataFile.size());
+              bp.print(F("Content-Type: text/plain"));
+              bp.println(F("Content-Disposition: attachment; filename: \"test.txt\""));
+              bp.println();
+              uint16_t c = 0;
+              while (dataFile.available()) {
+                bp.write(dataFile.read());
+              }
+              dataFile.close();
+              bp.flush();
               // uint8_t buf[256];
               // while (dataFile.available())
               // {
@@ -86,10 +95,19 @@ void loop() {
               //     //res->write(buf[i]);
               //   }
               // }
-              dataFile.close();
+              //dataFile.close();
                client.println();
              // output = "<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><script type=\"text/javascript\" src=\"jzip.js\"></script><script>var filelist = [";
-            }
+            } else { client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // Send your "Hello World" HTML response
+            client.println("<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
+            client.println("<body><p><a href=\"/download\"><button class=\"button button2\">Download</button></a></p></body></html>");
+            //client.println("<body><p><a href=\"test.txt\">Download!</a></p></body></html>");
+}
             // Break out of the while loop
             request = "";
             break;
@@ -111,7 +129,7 @@ void loop() {
 }
 
 void write_file(){
-    file = SPIFFS.open("/test.txt", FILE_WRITE);
+    file = SPIFFS.open("/test.csv", FILE_WRITE);
   if(!file){
     Serial.println("Error opening the file in WRITE mode");
     return;
@@ -121,17 +139,17 @@ void write_file(){
     Serial.println("Open file success");
   }
 
-  String testssid= "MY_WIFI";
-  String testpass = "MY_WIFI_PASSWORD";
+  String testline1= "a, b, c, d, e, f, g";
+  String testline2 = "1, 2, 3, 4, 5, 6, 7";
 
-  if(file.println(testssid))
+  if(file.println(testline1))
   {
-    Serial.println("SSID Written");
+    Serial.println("Line 1 written");
   }
 
-  if(file.println(testpass))
+  if(file.println(testline2))
   {
-    Serial.println("PASSWORD Written");
+    Serial.println("Line 2 Written");
   }
 
   file.close();
